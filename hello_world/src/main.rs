ca93eafc20cfe0239d983adb5a449de7edeb8ca5
enggsatyamraj <sattu62025@gmail.com>
2024-09-08 15:47:34 +0530

    initial commit

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("Hello, world!");
     std::thread::sleep(std::time::Duration::from_secs(10));
     println!("Goodbye after 10 sec");
+    println!("This is my first program in rust and I am loving it.")
 }
